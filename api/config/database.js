b3515f51f4920c5ded2871a3f9b931f2235850d7
loick.ducarme-at-773535489628 <loick.ducarme.sta@eureden.com>
2021-06-22 19:51:44 +0200

    ClearDB MySQL config

--- api/config/database.js
+++ api/config/database.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 const mysql = require("mysql"); 
 
 const connection = mysql.createConnection({
-	host     : process.env.DB_HOST,
-	user     : process.env.DB_USER,
-	password : process.env.DB_PASS,
-	database : process.env.DB_NAME
+	host     : process.env.NODE_ENV === "development" ? process.env.DB_HOST : process.env.DB_PROD_HOST,
+	user     : process.env.NODE_ENV === "development" ? process.env.DB_USER : process.env.DB_PROD_USER,
+	password : process.env.NODE_ENV === "development" ? process.env.DB_PASS : process.env.DB_PROD_PASS,
+	database : process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.DB_PROD_NAME
 });
 
 module.exports = connection;
