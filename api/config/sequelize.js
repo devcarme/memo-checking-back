09dabfc83e72ded2c9adbfa42d1b286640e45f08
loick.ducarme-at-773535489628 <loick.ducarme.sta@eureden.com>
2021-06-22 22:19:48 +0200

    config sequelize prod

--- api/config/sequelize.js
+++ api/config/sequelize.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
 	process.env.DB_PROD_USER, 
 	process.env.DB_PROD_PASS, 
 	{
-		host: process.env.NODE_ENV === "development" ? process.env.DB_HOST : process.env.DB_PROD_HOST,
+		host: process.env.DB_PROD_HOST,
 		dialect: "mysql"
 	});
 
