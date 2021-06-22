e728963675f7c18f3d4f5177e779b2e0b57519c0
loick.ducarme-at-773535489628 <loick.ducarme.sta@eureden.com>
2021-06-22 22:17:41 +0200

    config sequelize pour prod

--- api/config/sequelize.js
+++ api/config/sequelize.js
@@ -2,9 +2,9 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-	process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.DB_PROD_NAME, 
-	process.env.NODE_ENV === "development" ? process.env.DB_USER : process.env.DB_PROD_USER, 
-	process.env.NODE_ENV === "development" ? process.env.DB_PASS : process.env.DB_PROD_PASS, 
+	process.env.DB_PROD_NAME, 
+	process.env.DB_PROD_USER, 
+	process.env.DB_PROD_PASS, 
 	{
 		host: process.env.NODE_ENV === "development" ? process.env.DB_HOST : process.env.DB_PROD_HOST,
 		dialect: "mysql"
