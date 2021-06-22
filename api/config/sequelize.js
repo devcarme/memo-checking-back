6b39b5b26b092805599d1f27d71d911cde5df4fb
loick.ducarme-at-773535489628 <loick.ducarme.sta@eureden.com>
2021-06-22 21:08:33 +0200

    config sequelize pour prod

--- api/config/sequelize.js
+++ api/config/sequelize.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-undef */
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-	host: "localhost",
-	dialect: "mysql"
-});
+const sequelize = new Sequelize(
+	process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.DB_PROD_NAME, 
+	process.env.NODE_ENV === "development" ? process.env.DB_USER : process.env.DB_PROD_USER, 
+	process.env.NODE_ENV === "development" ? process.env.DB_PASS : process.env.DB_PROD_PASS, 
+	{
+		host: process.env.NODE_ENV === "development" ? process.env.DB_HOST : process.env.DB_PROD_HOST,
+		dialect: "mysql"
+	});
 
 try {
 	sequelize.authenticate();
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-irregular-whitespace */
-/* eslint-disable no-undef */
 const http = require("http");
 const app = require("./api/app");
 
@@ -14,7 +12,7 @@ const normalizePort = val => {
 	}
 	return false;
 };
-const port = normalizePort(process.env.PORT || 9000);
+const port = normalizePort(process.env.PORT || "9000");
 app.set("port", port);
 
 const errorHandler = error => {
